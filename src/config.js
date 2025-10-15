673da43e17f8455ca943480f3f6d9cce57af01eb
lukas söderlund <ls224ec@student.lnu.se>
2025-10-15 14:37:12 +0200

    config

---
+++ src/config.js
@@ -0,0 +1,19 @@
+export const TARGET_SCORES = Object.freeze({
+  level1: 30,
+  level2: 40,
+  level3: 50,
+  level4: 60,
+  level5: 75,
+  level6: 90,
+  level7: 110,
+  level8: 130
+})
+
+export const PLAYER_COLOR_HEX = '#ef4444'
+
+
+export const DIFFICULTY_PRESETS = Object.freeze({
+  easy:   { gravityForce: 0.45, movementSpeed: 7,  jumpStrengthValue: 12 },
+  normal: { gravityForce: 0.50, movementSpeed: 8,  jumpStrengthValue: 12 },
+  hard:   { gravityForce: 0.60, movementSpeed: 9,  jumpStrengthValue: 13 }
+})
